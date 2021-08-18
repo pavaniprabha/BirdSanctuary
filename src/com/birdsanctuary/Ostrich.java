187ee040040621f5bbb1def3ee3978c9c9a419c9
RAGOLUpavani <ragolupavani0330@gmail.com>
2021-08-18 07:13:43 +0530

    Bird Sanctuary Practice Problem

---
+++ src/com/birdsanctuary/Ostrich.java
@@ -0,0 +1,13 @@
+package com.birdsanctuary;
+
+
+public class Ostrich extends Bird {
+    public Ostrich(){
+        colour = Colour.GREY;
+    }
+
+    public void eat(){
+        System.out.println("Ostrich can eat");
+    }
+
+}
